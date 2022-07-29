1057e5bc89a8fe52d28dff870f78d72698d89649
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-29 12:13:11 +0530

    changes for colour shades and tints

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,index,hex}) => {
+  
+  const [alert,setAlert] = useState(false)
+  const bcg = rgb.join(',')
+
+  const hexValue = `#${hex}`
+  // const hexColor = rgbToHex(...rgb)
+
+  useEffect(()=>{
+    const timeOut = setTimeout(() => {
+      setAlert(false)
+    }, 3000);
+    return()=> clearTimeout(timeOut)
+  },[alert])
+
+  return (
+    <article onClick={()=>{
+      setAlert(true);
+      navigator.clipboard.writeText(hexValue)
+    }} className={`color ${index >= 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>Copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
