81b91e28d9c5d735673d423a14a5f2a1a66014a7
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-10 15:04:55 +0530

    Formatting

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb,weight,index,hex}) => {
-  
-  const [alert,setAlert] = useState(false)
+const SingleColor = ({ rgb, weight, index, hex }) => {
+
+  const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
 
   const hexValue = `#${hex}`
   // const hexColor = rgbToHex(...rgb)
 
-  useEffect(()=>{
+  useEffect(() => {
     const timeOut = setTimeout(() => {
       setAlert(false)
     }, 3000);
-    return()=> clearTimeout(timeOut)
-  },[alert])
+    return () => clearTimeout(timeOut)
+  }, [alert])
 
   return (
-    <article onClick={()=>{
+    <article onClick={() => {
       setAlert(true);
       navigator.clipboard.writeText(hexValue)
-    }} className={`color ${index >= 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}>
+    }} className={`color ${index >= 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
       {alert && <p className='alert'>Copied to clipboard</p>}
